13a90cc113ddc3d4ee3619a5200de5e8cb669f72
ukshedrack <ukshedrack@gmail.com>
2019-10-21 08:20:37 +0100

    Eloquent JS

--- Chapter4/exercise3.js
+++ Chapter4/exercise3.js
@@ -5,27 +5,12 @@ function arrayToList(array){
 	for (let i = array.length - 1; i >= 0; i--){
 		list = {value: array[i], rest: list};
 	}
-	return list
+	return list;
 }
-console.log(arrayToList([1,2,3]))
+console.log(arrayToList([1,2,3]));
 
-function listToArray(list){
-	let array = [];
-	for (let node = list; node; node = node.rest){
-		array.push(node.value)
-	}
-	return array
-}
-console.log(listToArray(arrayToList([1,2,3])))
 
 function prepend(value, list){
 	return {value, rest: list};
 }
-console.log(prepend(1, prepend(2, null)))
-
-function nth(list, n){
-	if (!list) return undefined;
-	else if (n == 0) return list.value
-	else return nth(list.rest, n - 1)
-}
-console.log(nth(arrayToList([1,2,3]), 2))
+console.log(prepend(1, prepend(2, null)));
